253b913c0c19b756925abcf26a15f55e67a988c3
CSarroca <sarroca93@gmail.com>
2022-07-16 17:44:45 +0200

    refactorized grid function to aim diferent objects

--- public/js/grid/grid.js
+++ public/js/grid/grid.js
@@ -1,11 +1,18 @@
+const Cell = require("../Cell/Cell");
+
 const gridGenerator = (columns, rows) => {
   const grid = [];
-  for (let row = 0; row < rows; row++) {
-    grid.push(new Array(columns).fill(2));
+  let newCell;
+
+  for (let i = 0; i < rows; i += 1) {
+    grid.push([]);
+    for (let j = 0; j < columns; j += 1) {
+      newCell = new Cell(false, i, j, 0);
+      grid[i].push(newCell);
+    }
   }
+
   return grid;
 };
 
-gridGenerator(10, 10);
-
-export default gridGenerator;
+module.exports = gridGenerator;
