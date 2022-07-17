4a46789f0735b9a934c2ed4b9769d63f7eca06d9
CSarroca <sarroca93@gmail.com>
2022-07-17 11:23:20 +0200

    correct neighborns variable return

--- public/js/checkCell/checkCell.js
+++ public/js/checkCell/checkCell.js
@@ -7,6 +7,7 @@ const checkCell = (row, column, grid) => {
   if (grid[row - 1][column - 1].alive === true) {
     aliveNeighbors += 1;
   }
+  return aliveNeighbors;
 };
 
 module.exports = checkCell;
