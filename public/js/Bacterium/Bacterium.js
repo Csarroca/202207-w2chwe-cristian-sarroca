02a04ffe9d4f8d6fa23b98298c065a26d0b219b0
CSarroca <sarroca93@gmail.com>
2022-07-17 13:04:28 +0200

    passed Bacterium class test ok

--- public/js/Bacterium/Bacterium.js
+++ public/js/Bacterium/Bacterium.js
@@ -4,12 +4,15 @@ class Bacterium {
   positionY;
   aliveNeighbors;
 
-  constructor(alive, positionX, positionY, aliveNeighbors) {
+  constructor(positionX, positionY, aliveNeighbors, alive = false) {
     this.alive = alive;
     this.positionX = positionX;
     this.positionY = positionY;
     this.aliveNeighbors = aliveNeighbors;
   }
 }
+
+const bacteriassss = new Bacterium();
+console.log(bacteriassss);
 
 module.exports = Bacterium;
